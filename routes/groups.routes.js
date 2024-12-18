f904490a61d6e9ec6ba18600c3fb20dcedeb95f8
neon2k21 <roman_2001@icloud.com>
2024-12-18 17:38:03 +0500

    update

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -1,6 +1,6 @@
 const Router = require('express')
 const router = new Router()
-const groupController = require('../controller/group.controller')
+const groupController = require('../controller/groups.controller')
 
 
 
--- routes/teachers.routes.js
+++ routes/teachers.routes.js
@@ -1,6 +1,6 @@
 const Router = require('express')
 const router = new Router()
-const teacherController = require('../controller/teacher.controller')
+const teacherController = require('../controller/teachers.controller')
 
 
 
